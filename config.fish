0d997ab1a994f3f020bf79552f31db9bdb52f28f
Niklas Kleiser <55552719+xXFiEsTaDeAmOnXx@users.noreply.github.com>
2023-03-17 21:57:56 +0100

    Added history command

--- config.fish
+++ config.fish
@@ -65,6 +65,35 @@ function up
   	end
 end
 
+# Functions needed for !! and !$
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+# The bindings for !! and !$
+if [ "$fish_key_bindings" = "fish_vi_key_bindings" ];
+  bind -Minsert ! __history_previous_command
+  bind -Minsert '$' __history_previous_command_arguments
+else
+  bind ! __history_previous_command
+  bind '$' __history_previous_command_arguments
+end
+
+
 ### ALIASES ###
 
 # Changing "ls" to "exa"
@@ -111,5 +140,3 @@ alias jctl="journalctl -p 3 -xb"
 
 neofetch
 starship init fish | source
-
-
